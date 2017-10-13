a90364866c6ad04ce59622e465284d4a53ce9cf1
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-13 11:36:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,8 @@
 # Write your code here.
-def line (katz_deli, name = " ")
+def line (katz_deli)
 if katz_deli.empty?
     puts "The line is currently empty."
 else
-    katz_deli << "#{name}"
     katz_deli.map.with_index {|name, index| puts "The line is currently: #{index + 1}. #{name} "}
   end
 end
